a8db6b1e1ad771e856e23d9b83fa75a7bc91fe5c
Giovane <giovaneaalmeida27@gmail.com>
2023-12-05 19:49:35 -0300

    feat: create TicketDetail

---
+++ app/tickets/[id]/page.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Ticket } from "../domain/ticket.interface";
+import { notFound } from "next/navigation";
+
+export async function generateStaticParams() {
+  const response = await fetch("http://localhost:4000/tickets");
+  const tickets: Ticket[] = await response.json();
+
+  return tickets.map((ticket) => ({
+    id: ticket.id,
+  }));
+}
+
+const getTicket = async (id: number) => {
+  const res = await fetch(`http://localhost:4000/tickets/${id}`, {
+    next: {
+      revalidate: 60,
+    },
+  });
+
+  if (!res.ok) {
+    notFound();
+  }
+
+  return res.json();
+};
+
+export default async function TicketDetails({ params }) {
+  const ticket: Ticket = await getTicket(params.id);
+  return (
+    <main>
+      <nav>
+        <h2>Ticket Details</h2>
+      </nav>
+      <div className="card">
+        <h3>{ticket.title}</h3>
+        <small>Created by {ticket.user_email}</small>
+        <p>{ticket.body}</p>
+        <div className={`pill ${ticket.priority}`}>
+          {ticket.priority} priority
+        </div>
+      </div>
+    </main>
+  );
+}
