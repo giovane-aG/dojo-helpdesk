7861d7ae9a6b69857f4123959500795b312575fb
Giovane <giovaneaalmeida27@gmail.com>
2023-12-19 16:20:56 -0300

    feat: create handleSubmit function

--- app/tickets/TicketList.tsx
+++ app/tickets/TicketList.tsx
@@ -23,14 +23,14 @@ export default async function TicketList() {
         <div key={ticket.id} className="card my-5">
           <Link href={`/tickets/${ticket.id}`}>
             <h3>{ticket.title}</h3>
-            <p>{ticket.body.slice(0, 200)}...</p>
+            <p>{ticket.body?.slice(0, 200)}...</p>
             <div className={`pill ${ticket.priority}`}>
               {ticket.priority} priority
             </div>
           </Link>
         </div>
       ))}
-      {tickets.length && (
+      {!tickets.length && (
         <p className="text-center">There are no open tickets</p>
       )}
     </>
