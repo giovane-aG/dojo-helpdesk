9dd47ef39c541fe4dcdf879879c3dbf968661602
Giovane <giovaneaalmeida27@gmail.com>
2023-12-19 15:56:27 -0300

    feat: create ticket component

---
+++ app/tickets/create/page.tsx
@@ -0,0 +1,10 @@
+import CreateForm from "./CreateForm";
+
+export default function CreateTicket() {
+  return (
+    <div>
+      <h2 className="text-primary text-center">Add New Ticket</h2>
+      <CreateForm />
+    </div>
+  );
+}
